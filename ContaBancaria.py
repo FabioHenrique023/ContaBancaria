e3a2b389d246d670f7bde9c09bf051c2f044d367
FabioHenrique023 <fabiohomeoffice904@gmail.com>
2023-09-05 17:45:37 -0300

    criando classe conta bancaria

---
+++ ContaBancaria.py
@@ -0,0 +1,6 @@
+class ContaBancaria:
+    def conta(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite 
